83770fa5299f2c18e4823fb3b40b222a8bbeffd8
Artem Sheremet <dot.doom@gmail.com>
2026-03-27 20:45:28 +0000

    Add nix substituters for internal network

--- modules/futureware.nix
+++ modules/futureware.nix
@@ -9,4 +9,15 @@
       "https://github.com/dotdoom/dotfiles.git";
     url."https://git.sheremet.ch/home/esphome.git".insteadOf = "https://github.com/dotdoom/esphome.git";
   };
+
+  nix.settings = {
+    substituters = [
+      "http://nix-cache.home.arpa"
+      # nix-community cachix server
+      # TODO: this, but through our cache -- "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
