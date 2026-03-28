ad2baea578edb86105fa81163b9bf192039f25d9
Artem Sheremet <dot.doom@gmail.com>
2026-03-28 20:31:10 +0000

    Revert nixpkgs path manipulation
    It doesn't work with modern nix restrictions, and it also didn't quite
    help the situation with "copying ... source".
    
    This reverts commit 2abb541c71e28763727641b243757ccff78d17c9.

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -1,26 +1,15 @@
-{ pkgs, ... }:
-{
-  nix = {
-    settings = {
-      extra-experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+_: {
+  nix.settings = {
+    extra-experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
-      auto-optimise-store = true;
+    auto-optimise-store = true;
 
-      trusted-users = [ "@wheel" ];
+    trusted-users = [ "@wheel" ];
 
-      # RPi builds can be slow due to compiling via binfmt.
-      download-buffer-size = 1 * 1024 * 1024 * 1024;
-    };
-
-    # Use nixpkgs of the current flake in "nix run", "nix shell" etc.
-    registry.nixpkgs.to = {
-      type = "path";
-      inherit (pkgs) path;
-    };
-    # Use nixpkgs of the current flake in nix-shell.
-    nixPath = [ "nixpkgs=${pkgs.path}" ];
+    # RPi builds can be slow due to compiling via binfmt.
+    download-buffer-size = 1 * 1024 * 1024 * 1024;
   };
 }
