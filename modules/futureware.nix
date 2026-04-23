24fddf5595576821aeda3ad7472a66093c73dec2
Artem Sheremet <dot.doom@gmail.com>
2026-04-23 21:36:10 +0000

    Add cuda-maintainers.cachix.org
    For e.g. pytorch with cuda support

--- modules/futureware.nix
+++ modules/futureware.nix
@@ -24,9 +24,11 @@ lib.mkMerge [
       substituters = [
         "http://nix-cache.home.arpa/cache.nixos.org"
         "http://nix-cache.home.arpa/nix-community.cachix.org"
+        "http://nix-cache.home.arpa/cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
 
