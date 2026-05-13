b6fb4221bd5f54bc427de84230e0c95952399c21
Artem Sheremet <dot.doom@gmail.com>
2026-05-13 20:51:31 +0000

    Switch from cuda-maintainers.cachix to nixos-cuda
    https://nixos.wiki/wiki/CUDA

--- modules/futureware.nix
+++ modules/futureware.nix
@@ -24,11 +24,11 @@ lib.mkMerge [
       substituters = [
         "http://nix-cache.home.arpa/cache.nixos.org"
         "http://nix-cache.home.arpa/nix-community.cachix.org"
-        "http://nix-cache.home.arpa/cuda-maintainers.cachix.org"
+        "http://nix-cache.home.arpa/cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
     };
 
