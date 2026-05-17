e6a7f3f4411da8b3ed47dde80f01be7304d7c88e
Artem Sheremet <dot.doom@gmail.com>
2026-05-17 09:02:19 +0000

    Specify times for nix-gc and optimise (assume UTC)

--- modules/nix-gc.nix
+++ modules/nix-gc.nix
@@ -1,7 +1,18 @@
-_: {
+{ lib, options, ... }:
+{
   nix.gc = {
     automatic = true;
-    dates = "weekly";
     options = "--delete-older-than 60d";
+  }
+  // lib.optionalAttrs (options.nix.gc ? dates) {
+    # NixOS
+    dates = lib.mkDefault "00:15";
+  }
+  // lib.optionalAttrs (options.nix.gc ? interval) {
+    # Darwin
+    interval = lib.mkDefault {
+      Hour = 0;
+      Minute = 15;
+    };
   };
 }
--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -1,4 +1,5 @@
-_: {
+{ lib, options, ... }:
+{
   nix.settings = {
     extra-experimental-features = [
       "nix-command"
@@ -11,7 +12,20 @@ _: {
     download-buffer-size = 1 * 1024 * 1024 * 1024;
   };
 
-  # Instead of nix.settings.auto-optimise-store, which adds overhead to each
-  # build.
-  nix.optimise.automatic = true;
+  nix.optimise = {
+    # Instead of nix.settings.auto-optimise-store, which adds overhead to each
+    # build.
+    automatic = true;
+  }
+  // lib.optionalAttrs (options.nix.optimise ? dates) {
+    # NixOS
+    dates = lib.mkDefault "00:45";
+  }
+  // lib.optionalAttrs (options.nix.optimise ? interval) {
+    # Darwin
+    interval = lib.mkDefault {
+      Hour = 0;
+      Minute = 45;
+    };
+  };
 }
